24b112934f2b8900a8b378fb530aa2f333739e92
rutvij-fsd <rutvijpatel414@gmail.com>
2022-12-03 15:00:27 +0530

    Add Remove Fav functionality added

--- JS/index.js
+++ JS/index.js
@@ -27,7 +27,13 @@ async function searchHeros(textSearched) {
 }
 
 function showSearchedResults(searchedHero) {
-     
+     let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+     if(favouritesCharacterIDs == null){
+          favouritesCharacterIDs = new Map();
+     }
+     else if(favouritesCharacterIDs != null){
+          favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+     }
      searchResults.innerHTML = ``;
      let count = 1;
      for (const key in searchedHero) {
@@ -49,7 +55,7 @@ function showSearchedResults(searchedHero) {
                               </div>
                          </div>
                          <div class="flex-col buttons">
-                              <button class="btn add-to-fav-btn"><i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites</button>
+                         <button class="btn add-to-fav-btn">${favouritesCharacterIDs.has(`${hero.id}`) ? "<i class=\"fa-solid fa-heart-circle-minus\"></i> &nbsp; Remove from Favourites" :"<i class=\"fa-solid fa-heart fav-icon\"></i> &nbsp; Add to Favourites</button>"}
                          </div>
                          <div style="display:none;">
                          <span>${hero.name}</span>
@@ -60,6 +66,7 @@ function showSearchedResults(searchedHero) {
                          <span>${hero.thumbnail.path+'/portrait_uncanny.' + hero.thumbnail.extension}</span>
                          <span>${hero.id}</span>
                          <span>${hero.thumbnail.path+'/landscape_incredible.' + hero.thumbnail.extension}</span>
+                         <span>${hero.thumbnail.path+'/standard_fantastic.' + hero.thumbnail.extension}</span>
                          </div>
                     </li>
                `
@@ -73,6 +80,90 @@ function events() {
      
      let characterInfo = document.querySelectorAll(".character-info");
      characterInfo.forEach((character) => character.addEventListener("click", addInfoInLocalStorage))
+
+     let favouriteButton = document.querySelectorAll(".add-to-fav-btn");
+     favouriteButton.forEach((btn) => btn.addEventListener("click", addToFavourites));
+}
+
+function addToFavourites() {
+     // console.log(this.parentElement.parentElement.children[2]);
+
+     if (this.innerHTML == '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites') {
+
+          let heroInfo = {
+               name: this.parentElement.parentElement.children[2].children[0].innerHTML,
+               description: this.parentElement.parentElement.children[2].children[1].innerHTML,
+               comics: this.parentElement.parentElement.children[2].children[2].innerHTML,
+               series: this.parentElement.parentElement.children[2].children[3].innerHTML,
+               stories: this.parentElement.parentElement.children[2].children[4].innerHTML,
+               portraitImage: this.parentElement.parentElement.children[2].children[5].innerHTML,
+               id: this.parentElement.parentElement.children[2].children[6].innerHTML,
+               landscapeImage: this.parentElement.parentElement.children[2].children[7].innerHTML,
+               squareImage: this.parentElement.parentElement.children[2].children[8].innerHTML
+          }
+
+          let favouritesArray = localStorage.getItem("favouriteCharacters");
+
+          if (favouritesArray == null) {
+               favouritesArray = [];
+          } else {
+               favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+          }
+
+          let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+
+          if (favouritesCharacterIDs == null) {
+               favouritesCharacterIDs = new Map();
+          } else {
+               // getting the map as object from localStorage and pasrsing it and then converting into map 
+               favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+               // favouritesCharacterIDs = new Map(Object.entries(favouritesCharacterIDs));
+          }
+
+          favouritesCharacterIDs.set(heroInfo.id, true);
+          console.log(favouritesCharacterIDs)
+
+          favouritesArray.push(heroInfo);
+
+
+          localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+          localStorage.setItem("favouriteCharacters", JSON.stringify(favouritesArray));
+
+          this.innerHTML = '<i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favourites';
+     }
+     else{
+
+          let idOfCharacterToBeRemoveFromFavourites = this.parentElement.parentElement.children[2].children[6].innerHTML;
+
+          let favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+          let favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+
+          let newFavouritesArray = [];
+          // let newFavouritesCharacterIDs = [];
+
+          favouritesCharacterIDs.delete(`${idOfCharacterToBeRemoveFromFavourites}`);
+
+          favouritesArray.forEach((favourite) => {
+               if(idOfCharacterToBeRemoveFromFavourites != favourite.id){
+                    newFavouritesArray.push(favourite);
+               }
+          });
+
+          // console.log(newFavouritesArray)
+
+          localStorage.setItem("favouriteCharacters",JSON.stringify(newFavouritesArray));
+          localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+
+          this.innerHTML = '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites';
+          // console.log();
+     }
+
+
+     // console.log(heroInfo.squareImage);
+
+
+
+
 }
 
 function addInfoInLocalStorage() {
@@ -86,7 +177,8 @@ function addInfoInLocalStorage() {
           stories: this.parentElement.parentElement.parentElement.children[2].children[4].innerHTML,
           portraitImage: this.parentElement.parentElement.parentElement.children[2].children[5].innerHTML,
           id: this.parentElement.parentElement.parentElement.children[2].children[6].innerHTML,
-          landscapeImage: this.parentElement.parentElement.parentElement.children[2].children[7].innerHTML
+          landscapeImage: this.parentElement.parentElement.parentElement.children[2].children[7].innerHTML,
+          squareImage: this.parentElement.parentElement.parentElement.children[2].children[8].innerHTML
      }
 
      localStorage.setItem("heroInfo", JSON.stringify(heroInfo));
--- JS/more-info.js
+++ JS/more-info.js
@@ -1,18 +1,29 @@
-let info = document.getElementById('info-container');
-let title = document.getElementById('page-title');
+let info = document.getElementById("info-container");
+let title = document.getElementById("page-title");
 
 let heroInfo = JSON.parse(localStorage.getItem("heroInfo"));
 
-title.innerHTML = heroInfo.name
+title.innerHTML = heroInfo.name;
 
-window.addEventListener("load",function(){
+window.addEventListener("load", function () {
+  let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+  if (favouritesCharacterIDs == null) {
+    favouritesCharacterIDs = new Map();
+  } else if (favouritesCharacterIDs != null) {
+    favouritesCharacterIDs = new Map(
+      JSON.parse(localStorage.getItem("favouritesCharacterIDs"))
+    );
+  }
 
-     info.innerHTML =
-          `
+  info.innerHTML = `
           <div class="flex-row hero-name">${heroInfo.name}</div>
                <div class="flex-row hero-img-and-more-info">
-                    <img id="portraitImage" class="hero-img" src="${heroInfo.portraitImage}" alt="">
-                    <img style="display:none;" id="landscapeImage" src="${heroInfo.landscapeImage}" alt="">
+                    <img id="portraitImage" class="hero-img" src="${
+                      heroInfo.portraitImage
+                    }" alt="">
+                    <img style="display:none;" id="landscapeImage" src="${
+                      heroInfo.landscapeImage
+                    }" alt="">
                     <div class="flex-col more-info">
                          <div class="flex-row id">
                               <b>ID:</b><span>${heroInfo.id}</span>
@@ -30,22 +41,141 @@ window.addEventListener("load",function(){
                </div>
                <div class="flex-col hero-discription">
                     <b>Discription:</b>
-                    <p>${heroInfo.description != "" ? heroInfo.description : "No Description Available"}</p>
+                    <p>${
+                      heroInfo.description != ""
+                        ? heroInfo.description
+                        : "No Description Available"
+                    }</p>
           </div>
-          `
+          <div style="display:none;">
+                    <span>${heroInfo.name}</span>
+                    <span>${heroInfo.portraitImage}</span>
+                    <span>${heroInfo.landscapeImage}</span>
+                    <span>${heroInfo.id}</span>
+                    <span>${heroInfo.comics}</span>
+                    <span>${heroInfo.series}</span>
+                    <span>${heroInfo.stories}</span>
+                    <span>${heroInfo.squareImage}</span>
+                    <span>${heroInfo.description}</span>
+          </div>
+               <button class="btn add-to-fav-btn">${
+                 favouritesCharacterIDs.has(`${heroInfo.id}`)
+                   ? '<i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favourites'
+                   : '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites</button>'
+               }
+          `;
+  addEvent();
+});
 
-})
+window.addEventListener("resize", function () {
+  let portraitImage = document.getElementById("portraitImage");
+  let landscapeImage = document.getElementById("landscapeImage");
 
-window.addEventListener('resize',function(){
-     let portraitImage = document.getElementById('portraitImage');
-     let landscapeImage = document.getElementById('landscapeImage');
+  // console.log(img);
+  if (document.body.clientWidth < 678) {
+    portraitImage.style.display = "none";
+    landscapeImage.style.display = "block";
+  } else {
+    landscapeImage.style.display = "none";
+    portraitImage.style.display = "block";
+  }
+});
 
-     // console.log(img);
-     if(document.body.clientWidth < 678){
-          portraitImage.style.display = "none";
-          landscapeImage.style.display = "block";
-     }else{
-          landscapeImage.style.display = "none";
-          portraitImage.style.display = "block";
-     }
-})
+function addEvent() {
+  let favouriteButton = document.querySelector(".add-to-fav-btn");
+  favouriteButton.addEventListener("click", addToFavourites);
+}
+
+function addToFavourites() {
+  // console.log(this.parentElement.parentElement.children[2]);
+
+  if (
+    this.innerHTML ==
+    '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites'
+  ) {
+    let heroInfo = {
+      name: this.parentElement.children[3].children[0].innerHTML,
+      description: this.parentElement.children[3].children[8].innerHTML,
+      comics: this.parentElement.children[3].children[4].innerHTML,
+      series: this.parentElement.children[3].children[5].innerHTML,
+      stories: this.parentElement.children[3].children[6].innerHTML,
+      portraitImage: this.parentElement.children[3].children[1].innerHTML,
+      id: this.parentElement.children[3].children[3].innerHTML,
+      landscapeImage: this.parentElement.children[3].children[2].innerHTML,
+      squareImage: this.parentElement.children[3].children[7].innerHTML,
+    };
+
+    let favouritesArray = localStorage.getItem("favouriteCharacters");
+
+    if (favouritesArray == null) {
+      favouritesArray = [];
+    } else {
+      favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+    }
+
+    let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+
+    if (favouritesCharacterIDs == null) {
+      favouritesCharacterIDs = new Map();
+    } else {
+      // getting the map as object from localStorage and pasrsing it and then converting into map
+      favouritesCharacterIDs = new Map(
+        JSON.parse(localStorage.getItem("favouritesCharacterIDs"))
+      );
+      // favouritesCharacterIDs = new Map(Object.entries(favouritesCharacterIDs));
+    }
+
+    favouritesCharacterIDs.set(heroInfo.id, true);
+    console.log(favouritesCharacterIDs);
+
+    favouritesArray.push(heroInfo);
+
+    localStorage.setItem(
+      "favouritesCharacterIDs",
+      JSON.stringify([...favouritesCharacterIDs])
+    );
+    localStorage.setItem(
+      "favouriteCharacters",
+      JSON.stringify(favouritesArray)
+    );
+
+    this.innerHTML =
+      '<i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favourites';
+  } else {
+    let idOfCharacterToBeRemoveFromFavourites =
+      this.parentElement.children[3].children[3].innerHTML;
+
+    let favouritesArray = JSON.parse(
+      localStorage.getItem("favouriteCharacters")
+    );
+    let favouritesCharacterIDs = new Map(
+      JSON.parse(localStorage.getItem("favouritesCharacterIDs"))
+    );
+
+    let newFavouritesArray = [];
+    // let newFavouritesCharacterIDs = [];
+
+    favouritesCharacterIDs.delete(`${idOfCharacterToBeRemoveFromFavourites}`);
+
+    favouritesArray.forEach((favourite) => {
+      if (idOfCharacterToBeRemoveFromFavourites != favourite.id) {
+        newFavouritesArray.push(favourite);
+      }
+    });
+
+    // console.log(newFavouritesArray)
+
+    localStorage.setItem(
+      "favouriteCharacters",
+      JSON.stringify(newFavouritesArray)
+    );
+    localStorage.setItem(
+      "favouritesCharacterIDs",
+      JSON.stringify([...favouritesCharacterIDs])
+    );
+
+    this.innerHTML =
+      '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites';
+    // console.log();
+  }
+}
